88e86b1a6020a1d59c7e88a41f5cd56f5f8f8167
manaratef570 <58149620+manaratef570@users.noreply.github.com>
2022-07-12 21:35:41 +0200

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,70 @@
+import 'package:corner_ar_gp/authentication/registration/registration_screen.dart';
+import 'package:corner_ar_gp/main_screens/home_screen/admin_homescreen.dart';
+import 'package:corner_ar_gp/main_screens/home_screen/user_homescreen.dart';
+import 'package:corner_ar_gp/provider_manager/AppProvider.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'authentication/login/LoginPage.dart';
+import 'main_screens/home_screen/loading_screen.dart';
+
+
+/** debug token D7C96657-48AD-422C-BEE2-49A12E387376  **/
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // await FirebaseAppCheck.instance.activate(
+  //   webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+  // );
+
+  await FirebaseAppCheck.instance.activate();
+  String? token = await FirebaseAppCheck.instance.getToken();
+  print("------------===========================----------- $token");
+  await FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true);
+  FirebaseAppCheck.instance.onTokenChange.listen((token) {
+  print("==========================>>>>>>>>>>>>>>>> $token");
+  });
+
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    return ChangeNotifierProvider(
+      create: (context)=> AppProvider(),
+      builder: (context,widget) {
+        final myAppProvider = Provider.of<AppProvider>(context);
+        final isUserLoggedIn = myAppProvider.checkLoggedUser();
+        bool isAdmin = false;
+        if(isUserLoggedIn){
+          myAppProvider.fetchLoggedUser();
+          // isAdmin = myAppProvider.isLoggedUserAdmin();
+          // print("===>  $isAdmin");
+        }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          routes: {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            RegistrationScreen.routeName: (context) =>
+                RegistrationScreen(isAdmin: false),
+            Login.routeName: (context) => Login(),
+            UserHomeScreen.routeName: (context) => UserHomeScreen(),
+            AdminHomeScreen.routeName: (context) => AdminHomeScreen(),
+            LoadingScreen.routeName: (context) =>LoadingScreen()
+          },
+          initialRoute: isUserLoggedIn?
+              //isAdmin? AdminHomeScreen.routeName : UserHomeScreen.routeName
+          LoadingScreen.routeName
+          : Login.routeName,
+        );
+      }
     );
   }
 }
